0785b1af37c62ff5e3de02faa70f3edb953dff10
seomoonjeong <slow.seo4612@gmail.com>
2025-10-17 10:22:34 +0900

    feat:20251017 Promise

---
+++ typescriptstudy/src/App3.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import HelloComponent from './HelloComponent'
+import ByeComponent from './ByeComponent'
+
+function App() {
+  
+
+  return (
+    <>
+      <HelloComponent name='김일' age={20} />
+      <br />
+      <br />
+      <ByeComponent name='김일' />
+    </>
+  )
+}
+
+export default App
